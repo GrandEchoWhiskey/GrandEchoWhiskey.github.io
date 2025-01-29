a1a248a2910b6d22673cffec6e13bbbba131e49d
Eric <102000288+GrandEchoWhiskey@users.noreply.github.com>
2025-01-29 18:03:05 +0100

    test

--- data.js
+++ data.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
             data.forEach(item => {
                 const card = document.createElement("div");
-                card.classList.add("card", "col-md-4", "col-xl-3", "my-2");
+                item.class.split(" ").forEach(x => card.classList.add(x));
 
                 card.innerHTML = `
                     <div class="card-body">
                         <a href="${item.link}" target="_blank">
-                            <img src="${item.image}" class="card-img-top profile mx-auto d-block my-3" alt="${item.title}"/>
+                            <img src="${item.image}" class="${item.img_class}" alt="${item.title}"/>
                         </a><hr>
                         <h5 class="card-title">${item.title}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">${item.subtitle}</h6>
