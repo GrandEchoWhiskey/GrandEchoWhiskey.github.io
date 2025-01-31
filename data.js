0a8917ab7d9c04435c01b8a86862e69a9165c8fa
Eric <102000288+GrandEchoWhiskey@users.noreply.github.com>
2025-01-31 17:48:22 +0100

    Added data over json

--- data.js
+++ data.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 const card = document.createElement("div");
                 item.class.split(" ").forEach(x => card.classList.add(x));
 
+                var url = window.location.href.toLowerCase();
+
                 card.innerHTML = `
                     <div class="card-body">
                         <a href="${item.link}" target="_blank">
@@ -21,7 +23,13 @@ document.addEventListener("DOMContentLoaded", function () {
                     </div>
                 `;
 
-                container.appendChild(card);
+                if (url.includes("index.html")) {
+                    container.appendChild(card);
+                } else {
+                    if (item.category == url.split("/").pop().split(".")[0]) {
+                        container.appendChild(card);
+                    }
+                }
             });
         })
         .catch(error => console.error("Error loading data:", error));
